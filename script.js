2cbcdf7b10857af44307dc104fd965a05d5f1e2e
Renato Martins <91312588+DryGut@users.noreply.github.com>
2022-09-12 15:56:41 +0000

    refactored

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
-const showPokemon = document.getElementById('fullname');
-
-const buscarPokemon = document.getElementById('buscar');
-const inputBuscar = document.createElement('input');
-inputBuscar.classList.add('nome-pokemon');
-inputBuscar.setAttribute('type', 'text');
-buscarPokemon.appendChild(inputBuscar);
-
-const pokemonTag = document.createElement('h2');
-pokemonTag.classList.add('status-pokemon');
-const pokemonImage = document.createElement('img');
-pokemonImage.setAttribute('src', '#');
-pokemonImage.classList.add('pokeimage');
-
-const btnBuscar = document.createElement('button');
-btnBuscar.setAttribute('type', 'button');
-btnBuscar.innerHTML = 'Buscar';
-buscarPokemon.appendChild(btnBuscar);
+const objectHtml = {
+  buttonToSearch: document.querySelector('.buscar'),
+  nameOfPokemon: document.querySelector('.nome-pokemon'),
+  statusOfPokemon: document.querySelector('.status-pokemon'),
+  pokeImage: document.querySelector('.pokeimage'),
+}
 
 
-async function getPoke(pokemon){
-  const SOME_API = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-  const response = await fetch(SOME_API);
-  if(response.status == 200){
+async function choosePokemon(pokemon) {
+  const pokeAPI = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+  const response = await fetch(pokeAPI);
+  if (response.status === 200) {
     const data = await response.json();
-    pokemonImage.src = await data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    pokemonTag.innerHTML = 
-      "Nome: " + data.name +
-      "<br>Tipo: " + data['types'][0]['type']['name']+
-      "<br>Hp: " + data['stats'][0]['base_stat']+
-      "<br>Attack: " + data['stats'][1]['base_stat']+
-      "<br>Defense: " + data['stats'][2]['base_stat'];
-    showPokemon.appendChild(pokemonImage);
-    showPokemon.appendChild(pokemonTag);
+    return data;
   }
 }
 
-btnBuscar.addEventListener('click',(event =>{
+const showPokemon = async(pokemon)=>{
+  const data = await choosePokemon(pokemon);
+  const pokeStatus = [`Nome: ${data.name}`, 
+                      `Tipo: ${data['types']['0']['type']['name']}`,
+                      `Hp: ${data['stats']['0']['base_stat']}`,
+                      `Attack: ${data['stats']['1']['base_stat']}`,
+                      `Defense: ${data['stats']['2']['base_stat']}`];
+  for(let value of pokeStatus){
+    const pokeList = document.createElement('li');
+    pokeList.innerHTML = value;
+    objectHtml.statusOfPokemon.appendChild(pokeList);
+  }
+  objectHtml.pokeImage.src = 
+    await data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+}
+
+
+objectHtml.buttonToSearch.addEventListener('click',(event =>{
   event.preventDefault();
-  getPoke(inputBuscar.value);
+  showPokemon(objectHtml.nameOfPokemon.value);
 }));
